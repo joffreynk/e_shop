1b1543266a87fe5895bce047d767656662b9e5d4
joffreynk <joyjoffrey@gmail.com>
2022-10-21 19:47:40 +0200

    add testing to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,7 @@ class Category < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :join_category_with_shoping_lists, dependent: :destroy
   has_many :shoping_lists, through: :join_category_with_shoping_lists
-  validates :name, :icon, presence: true, length: { minimum: 2 }
+  validates :name, :icon, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :icon, length: { minimum: 10 }
 end
