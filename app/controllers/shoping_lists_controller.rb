229f2307ed67662a96e37c1f2967aab3266bd315
joffreynk <joyjoffrey@gmail.com>
2022-10-21 11:24:00 +0200

    add navbar and and fix displaying data

--- app/controllers/shoping_lists_controller.rb
+++ app/controllers/shoping_lists_controller.rb
@@ -10,7 +10,7 @@ class ShopingListsController < ApplicationController
   end
   
   def create
-    @item = ShopingList.create(name: params[:name], amount: params[:amount], user_id: current_user.id)
+    @item = ShopingList.create(name: params[:name], amount: params[:amount],icon:params[:icon_link], user_id: current_user.id)
     JoinCategoryWithShopingList.create(category_id: params[:brand_id], shoping_list_id: @item.id)
     redirect_to "/categories/#{params[:brand_id]}/shopinglists"
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -245,7 +245,7 @@ Devise.setup do |config|
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
   config.scoped_views = true
-
+  config.navigational_formats = ['*/*', :html, :turbo_stream] 
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
